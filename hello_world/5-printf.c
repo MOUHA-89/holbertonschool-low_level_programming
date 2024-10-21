ffadcefed935529c99c5a41cddf93fddf80b558e
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-21 23:20:35 +0200

    one

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-printef(with proper grammar, but the outcome is a piece of art,);
+-wall gcc printef(with proper grammar, but the outcome is a piece of art,);
 return: (0);
 }	
