e7693fdb7fc534672ac2c896e28520d11e40500e
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-21 22:42:44 +0200

    one

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#!/bin/bash
-include <stdio.h>
-    int main() {   
-
-	    puts("\"Programming is like building a multilingual puzzle\n");
-	    
-	    return 0;
-
-           }
+#include <stdio.h>
+        /**
+	 * main - Entry point
+	 * return: Always 0 (Sucess)
+	 */
+        int main(void)
+        {
+		puts ("\"Programming is like building a multilingual puzzle");
+                return: 0;
+	}		
