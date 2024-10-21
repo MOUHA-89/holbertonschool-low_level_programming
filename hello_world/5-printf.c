d8ad85997069d9359167f36b2d6f97251bae363b
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-21 23:28:35 +0200

    one

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio>
 /**
  * main - main block
- * Return: 0
+ * Return : 0
  */
 int main(void)
 {
--wall gcc printef(with proper grammar, but the outcome is a piece of art,);
-return: (0);
+-wall gcc printef(with proper grammar, but the outcome is a piece of art ,);
+return : (0);
 }	
