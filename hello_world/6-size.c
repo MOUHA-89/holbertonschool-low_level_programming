8e3cf83003dcf5c3fcd80fa57dfbba28704ccbd3
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-22 14:57:08 +0200

    one

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - main block
+ * Return: 0
  * Size of an int: 4 bytes
  * Size of a float: 4 bytes
  * Size of a long long int: 8 bytes
  * Size of a long int: 4 bytes
  * Size of a char: 1 byte
- * Return: 0
  */
 int main(void)
 {
