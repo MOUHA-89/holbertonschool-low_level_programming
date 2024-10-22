52ca7c4fb1d54c23ba33a7a652db5090424deb6f
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-22 13:11:35 +0200

    one

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - C program to find size of int,float,double and char data types using sizeof operator directly
- * determine and print the size of int
- * determine and imprime the size of float
- * determine and imprime the size of double
- * determine and imprime the size of char
+ * determine and imprime size of int: 4
+ * determine and imprime the size of float: 4
+ * determine and imprime the size of double: 8
+ * determine and imprime the size of char: 1
  * Return: 0
  */
 int main(void)
