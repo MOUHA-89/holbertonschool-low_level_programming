33b5a8081423348367e0faf472ae4050880b34f9
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-22 14:52:45 +0200

    one

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
  * main - main block
- * Return: 0
  * Size of an int: 4 bytes
  * Size of a float: 4 bytes
  * Size of a long long int: 8 bytes
  * Size of a long int: 4 bytes
  * Size of a char: 1 byte
+ * Return: 0
  */
 int main(void)
 {
 printf("Size of an int: %lu bytes\n", sizeof(int));
 printf("Size of a float: %lu bytes\n", sizeof(float));
-printf("Size of a long long: %lu bytes\n", sizeof(long long));
+printf("Size of a long long int: %lu bytes\n", sizeof(long long int));
 printf("Size of a long int: %lu bytes\n", sizeof(long int));
 printf("Size of a char: %lu bytes\n", sizeof(char));
 return (0);
