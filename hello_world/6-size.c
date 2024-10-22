359638878e1c163e14496b341a1c081c6c3a7aad
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-22 14:07:13 +0200

    one

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,9 +9,9 @@
  */
 int main(void)
 {
-printf("Size of int: %u bytes\n", sizeof (int));
-printf("Size of float: %u bytes\n", sizeof (float));
-printf("Size of double: %u bytes\n", sizeof (double));
-printf("Size of char: %u bytes\n", sizeof (char));
+printf("Size of int: %u bytes\n", sizeof(int));
+printf("Size of float: %u bytes\n", sizeof(float));
+printf("Size of double: %u bytes\n", sizeof(double));
+printf("Size of char: %u bytes\n", sizeof(char));
 return (0);
 }
