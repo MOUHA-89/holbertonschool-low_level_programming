5e708697c612f911cd8aa56a0e175577e5845821
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-22 14:45:15 +0200

    one

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,10 +10,10 @@
  */
 int main(void)
 {
-printf("Size of an int: %u bytes\n", sizeof(int));
-printf("Size of a float: %u bytes\n", sizeof(float));
-printf("Size of a long long: %u bytes\n", sizeof(long long));
-printf("Size of a long int: %u bytes\n", sizeof(long int));
-printf("Size of a char: %u bytes\n", sizeof(char));
+printf("Size of an int: %lu bytes\n", sizeof(int));
+printf("Size of a float: %lu bytes\n", sizeof(float));
+printf("Size of a long long: %lu bytes\n", sizeof(long long));
+printf("Size of a long int: %lu bytes\n", sizeof(long int));
+printf("Size of a char: %lu bytes\n", sizeof(char));
 return (0);
 }
