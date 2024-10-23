b2b152245a3261d89e09dc1e2c938417ceae8918
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-23 22:57:46 +0200

    one

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,12 @@ int main(void)
 char alphabet = 'a';
 while (alphabet <= 'z')
 {
-if (alphabet == 'q' || alphabet == 'e')
+if (alphabet != 'q' && alphabet != 'e')
 {
-continue;
-}
 putchar(alphabet);
+}
 alphabet++;
-{
+}
 putchar('\n');
 return (0);
 }
