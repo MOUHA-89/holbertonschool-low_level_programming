5eb826ebc4c7b554aab4b624dc68be4e4d4d7778
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-23 17:09:41 +0200

    one

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+boucle [a - z] is alphabet
+char alphabet = 'a'
+while alphabet <= 'z'
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
