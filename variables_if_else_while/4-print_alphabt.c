ac28a0eaacfcbce3cf75c7ac474e804fe6a470c6
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-23 22:01:37 +0200

    one

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+if (alphabet == 'q' || alphabet == 'e')
+{
+continue;
+}
+putchar(alphabet);
+alphabet++;
+{
+putchar('\n');
+return (0);
+}
