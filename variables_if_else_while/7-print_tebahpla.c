cfb4cbc21c03cdf56401e708cd3f2c09684f5d3c
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-24 16:39:30 +0200

    update file 7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -3,18 +3,15 @@
  * main - main block
  * Return: 0
  */
-
 int main(void)
 {
-char alphabet = 'z';
-
-while (alphabet <= 'a')
 {
-putchar(alphabet);
-
-alphabet++;
-}
+{
+char n;
+for (n = 'z'; n >= 'a'; n--)
+putchar(n);
 putchar('\n');
-
 return (0);
+}
+}
 }
