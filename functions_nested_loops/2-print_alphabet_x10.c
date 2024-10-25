a60c4683bd0c9efec934699a0480a4b1b7697cf4
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-25 16:52:15 +0200

    one

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - main block
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+char alphabetx10 = 'a';
+for (; alphabet_x10 <= 'z'; alphabet_x10++)
+{
+_putchar(alphabet_x10);
+}
+_putchar('\n');
+{
