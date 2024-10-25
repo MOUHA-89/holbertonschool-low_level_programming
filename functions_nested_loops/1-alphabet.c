f88560a4a63b539286e715e55b91f3b223bd6298
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-25 14:34:56 +0200

    prototype

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+char alphabet = 'a';
+for (; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+return (0);
+}
