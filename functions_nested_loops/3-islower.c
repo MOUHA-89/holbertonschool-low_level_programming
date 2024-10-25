61d1e48f11760604427fd4d6cae9261569ae7982
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-25 22:53:42 +0200

    one

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * Return: 0
+ */
+int _islower(int c)
+{
+int i;
+char k = 'a';
+for ( ; k <= 'z'; k++)
+if (i == 1)
+{
+putchar(k);
+}
+else
+{
+return (0);
+}
