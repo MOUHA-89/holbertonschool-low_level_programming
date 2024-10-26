a3014b2333838d51799bfd0e92422c02c2183040
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-26 02:34:14 +0200

    one

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,24 +1,24 @@
 #include "main.h"
-
 /**
  * print_sign - controls
- * n:is a number
+ * @n:is a number
  * Return:  0
  */
 int print_sign(int n)
 {
 if (n > 0)
-{	
-putchar(+);
-return: 1;
-}
-if (n == 0)
 {
-putchar(0);
-return: 0;
+_putchar('+');
+return (1);
 }
-if (n < 0)
+else if (n == 0)
 {
-putchar(-);
-return: -1;
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 }
