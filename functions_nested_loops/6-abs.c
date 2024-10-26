9fea073e62fa863f21401ed6bc6f242304db3145
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-26 14:10:51 +0200

    one

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - print absolute value
+ * @a: is number
+ * Return: 0
+ */
+int _abs(int a)
+{
+if (a > 0)
+{
+return (a);
+}
+else if (a < 0)
+{
+return (-a);
+}
+else
+{
+return (0);
+}
+}
