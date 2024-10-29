2013d2e94fa40f629e00a06b27e0551e4eda7fc3
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-29 15:34:50 +0100

    one

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,7 +8,11 @@ void print_square(int size)
 {
 int a;
 int b;
-if (size > 0)
+if (size < 0)
+{
+_putchar('\n');
+}
+else
 {
 for (a = 0; a < size; a++)
 {
@@ -16,10 +20,6 @@ for (b = 0; b < size; b++)
 {
 _putchar('#');
 }
-_putchar('\n');
-}
-else
-{
 _putchar('\n');
 }
 }
