f50b59591c9eea90ca0bfb61a8fbf3647f51d432
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-29 14:44:12 +0100

    Update file 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - print_square block
+ * @size: is a parameter
+ * Return: 0
+ */
+void print_square(int size)
+{
+int a;
+int b;
+
+if (size > 0)
+{
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+else
+_putchar('\n');
+}
