4738196cb4d6a6c3152527fdd1f986829630f16e
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-10-30 10:47:17 +0100

    one

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+int a = 1;
+while (a <= 100)
+{
+if (a % 3 == 0 && a % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (a % 5 == 0)
+{
+printf("Buzz");
+}
+else if (a % 3 == 0)
+{
+printf("Fizz");
+}
+else
+{
+printf("%d", a);
+}
+if (a < 100)
+{
+printf(" ");
+}
+a++;
+}
+printf("\n");
+return (0);
+}
