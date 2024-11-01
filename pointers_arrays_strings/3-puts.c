a77c0936108e9eaca4114e7f44f3bf221f8d9666
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-11-01 18:43:07 +0100

    one

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
  * _puts -  _puts block
- * @str: is a size
+ * @str: is a string
  * Return: return void
  */
 void _puts(char *str)
 {
-int a;
+int a = 0;
 while (str[a] != '\0')
 {
 _putchar(str[a]);
 a++;
 }
+
 _putchar('\n');
 }
 
