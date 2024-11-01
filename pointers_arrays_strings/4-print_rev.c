1c4871a5a5a1a84c468eea4bad43764e47367cff
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-11-01 19:13:00 +0100

    one

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - print_rev block
+ * @s: is a size
+ * Return: return void
+ */
+void print_rev(char *s)
+{
+int a;
+while (s[a] != '\0')
+{
+_putchar(s[a] != '\0');
+a++;
+}
+_putchar('\n');
+}
