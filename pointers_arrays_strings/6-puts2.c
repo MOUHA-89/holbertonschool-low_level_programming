278414f8278c34a4369b9cdb89546ee9ee2759a9
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-11-02 10:16:42 +0100

    one

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts2 - puts2 block
+ * @str: is a size
+ * Return: return void
+ */
+void puts2(char *str)
+{
+char *y = str;
+int a = 0;
+int b = 0;
+int c;
+while (*y != '\n')
+{
+y++;
+a++;
+}
+b = a - 1;
+for (c = 0; c <= b; c++)
+{
+if (0 % 2 == 0)
+{
+_putchar(str[c]);
+}
+_putchar('\n');
+}
+}
