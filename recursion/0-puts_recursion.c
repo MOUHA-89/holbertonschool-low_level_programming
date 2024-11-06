c13a2e8cff4879669ce892d8554afb4214cb8557
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-11-06 19:49:16 +0100

    special one

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -6,10 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-int x;
-for (x = 0; s[x] != '\0'; x++)
+if (*s == '\0')
 {
-_putchar(s[x]);
-}
 _putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
 }
