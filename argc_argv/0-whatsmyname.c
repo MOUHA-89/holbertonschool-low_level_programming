94630cd1af8e6cb4de64f96c1b9694d990bfb0c4
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-11-08 23:48:34 +0100

    special one

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * @argc: are arguments
+ * @argv: is a arrea string
+ * Return: 0
+ */
+int main(int argc, char* argv[])
+{
+int nombre;
+for (nombre = 0; nombre < argc; nombre++)
+printf("argv[%d] = %s", nombre, argv[nombre]);
+return (0);
+}
