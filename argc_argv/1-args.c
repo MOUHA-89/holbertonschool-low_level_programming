9e51cf56e196891fb08341949f20d1cbeb66e7db
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-11-09 14:26:44 +0100

    special one

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: is arrea string
  * Return: 0
  */
-int main(int argc,char __attribute__((__unused__)) *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 printf("%d\n", argc - 1);
 return (0);
