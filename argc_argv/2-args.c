0f5011959c036b7825a9eb4f568be502a8e23c5d
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-11-09 14:56:00 +0100

    special one

---
+++ argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * @argc: number of arguments
+ * @argv: arrea string
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int nombre;
+for (nombre = 0; nombre < argc; nombre++)
+printf("argv[%d] = %s\n", nombre, argv[nombre]);
+return (0);
+}
