92a0e152cad920282312fc4d846c3b3907671f4f
MOUHA-89 <mouhamedkeita05@gmail.com>
2024-11-22 18:28:00 +0100

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 /**
  * print_all -  function that prints anything
- * @format: is a parameter
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
